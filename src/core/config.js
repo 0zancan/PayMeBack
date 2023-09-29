9b9116e9d00cc3ec49443279a5a938208b107d25
Ozan Can <48280981+0zancan@users.noreply.github.com>
2023-09-29 10:17:26 +0300

    first commit

---
+++ src/core/config.js
@@ -0,0 +1,10 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAMlp6KFfadflYa0Sf02ItUNayDbqyAUzY",
+  authDomain: "paymeback-68bd8.firebaseapp.com",
+  projectId: "paymeback-68bd8",
+  storageBucket: "paymeback-68bd8.appspot.com",
+  messagingSenderId: "707798153927",
+  appId: "1:707798153927:web:e79620cade8c3f378cdce2",
+  measurementId: "G-1CS8P8576K"
+}
